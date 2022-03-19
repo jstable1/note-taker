47dff63ce1c8e9d6533116252f94bf6ef1871a93
jstable1 <jessica.stabler@gmail.com>
2022-03-18 21:09:45 -0600

    Add Heroku

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const { notes } = require('./db/db.json');
+const fs = require('fs');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+});
+
+app.use(express.static('public'));
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
